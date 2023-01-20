79c5ff7d8e7137de82e6238b6b4a05b8b5d4903d
abdjama <abdullahijama101@gmail.com>
2023-01-20 01:55:09 -0800

    fixed button IDs that were capitalized and added some JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let inputField = document.getElementById("input");
 let listContainer = document.getElementById("listContainer");
 let add = document.getElementById("add");
 let remove = document.getElementById("remove");
+
+add.addEventListener("click", addItem);
+
+function addItem() {
+    let item = inputField.value;
+    console.log(item);
+    listContainer.innerHTML = item
+}
