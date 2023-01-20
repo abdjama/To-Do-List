b4f1e88ec745ece09c415b8ca0001c22f4e2dee1
abdjama <abdullahijama101@gmail.com>
2023-01-20 02:36:23 -0800

    testing js

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ let inputField = document.getElementById("input");
 let listContainer = document.getElementById("listContainer");
 let add = document.getElementById("add");
 let remove = document.getElementById("remove");
+let count = 0;
+
 
 add.addEventListener("click", addItem);
 
 function addItem() {
+    count++
     let item = inputField.value;
-    console.log(item);
+    let p = document.createElement("p");
+    p.setAttribute("count", count)
+    p.value = item
     listContainer.innerHTML += `<p>${item}</p>`;
+    inputField.value = "";
 }
 
